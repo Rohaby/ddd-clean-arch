7e6ec49d452eb023f97521c990ea761032a377d7
Rohaby <ro.klauber@gmail.com>
2026-04-06 20:19:02 -0300

    Foi criado o formulario PacienteForm visando a criação e ediçãode um paciente, unindo os componentes DocumentoForm, EnderecoForm e TelefoneForm. Ainda requer ajustes no botão salvar

---
+++ src/src/modules/Paciente/Components/PacienteForm.tsx
@@ -0,0 +1,53 @@
+"User Cliente"
+
+import { DocumentoForm } from "@/modules/Documento/Components/DocumentoForm";
+import { useState } from "react"
+import { EnderecoForm } from "../../Endereco/Components/EnderecoForm";
+import { TelefoneForm } from "../../Telefone/Components/TelefoneForm";
+
+export function PacienteForm({onSubmit} : any){
+
+    const[nome, setNome] = useState ("");
+    const[genero, setGenero] = useState("");
+    const[idade, setIdade] = useState ("");
+    const[peso, setPeso] = useState ("");
+    const[altura, setAltura] = useState ("");
+
+    const[documento, setDocumento] = useState<any>(null);
+    const[endereco, setEndereco] = useState<any>(null);
+    const[telefone, setTelefone] = useState<any>(null);
+
+
+    return(
+        <div>
+
+       Nome: <input type="text" value={nome} onChange={e => setNome(e.target.value)} /><br/>
+       Genero: <input type="text" value={genero} onChange={e => setGenero(e.target.value)} /><br/>
+       Idade: <input type="text" value={idade} onChange={e => setIdade(e.target.value)} /><br/>
+       Peso: <input type="text" value={peso} onChange={e => setPeso(e.target.value)} /><br/>
+       Altura: <input type="text" value={altura} onChange={e => setAltura(e.target.value)} /><br/>
+
+       <hr/>
+
+       <DocumentoForm onSubmit={(numeroDocumento: string, tipoDocumento: string) => {
+        setDocumento({numeroDocumento, tipoDocumento});
+       }} />
+
+
+        <EnderecoForm onSubmit={(logradouro: string, numero: number, bairro: string, cidade: string, estado:string) => {
+        setEndereco({logradouro, numero, bairro, cidade, estado})
+       }} />
+
+
+       <TelefoneForm onSubmit={(ddd: string, numeroTelefone: string, tipoTelefone: string, ativo: boolean) => {
+        setTelefone({ddd, numeroTelefone, tipoTelefone, ativo})
+       }} />
+
+
+       <button type="submit"> Salvar telefone </button>
+
+
+        </div>
+    );
+
+}
---
+++ src/src/modules/Paciente/Domain/Paciente.ts
@@ -0,0 +1,70 @@
+import { Endereco } from "@/modules/Endereco/Domain/Endereco";
+import { Usuario } from "@/modules/Usuario/Domain/Usuario";
+
+
+export class Paciente extends Usuario {
+
+    private _idade: number;
+    private _peso: number;
+    private _altura: number;
+    private _endereco: Endereco;
+
+    constructor(id: number, nome: string, genero: string, idade: number, peso: number, altura: number, idDocumento: number, numeroDocumento: string, tipoDocumento: string, endereco: Endereco) {
+        super(id, nome, genero, idDocumento, numeroDocumento, tipoDocumento);
+
+        this._idade = idade;
+        this._peso = peso;
+        this._altura = altura;
+        this._endereco = endereco;
+
+        // this.prontuario = new Prontuario(this);
+    }
+
+    get id(){
+        return super.id;
+    }
+
+    
+    get idade() {
+        return this._idade;
+    }
+
+    get peso() {
+        return this._peso;
+    }
+
+    get altura() {
+        return this._altura;
+    }
+
+    get endereco() {
+        return this._endereco;
+    }
+
+
+    set idade(idade: number) {
+        this._idade = idade;
+    }
+
+    set peso(peso: number) {
+        this._peso = peso;
+    }
+
+
+    set altura(altura: number) {
+        this._altura = altura;
+    }
+
+    set endereco(endereco: Endereco) {
+        this._endereco = endereco;
+    }
+
+    toString() {
+        return super.toString() + `
+        Idade: ${this._idade}
+        Peso: ${this._peso}
+        Altura: ${this._altura}
+
+        ${this._endereco.toString()}`;
+    }
+}
